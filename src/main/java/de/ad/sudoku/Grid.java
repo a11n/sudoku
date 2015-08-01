a6bc8ab7729a878e31f26050dc5c01b87263b9c5
a11n <andre.diermann@gmail.com>
2015-08-01 13:30:38 +0200

    Removed Printer class. Moved its functionality to Grid#toString().

--- src/main/java/de/ad/sudoku/Grid.java
+++ src/main/java/de/ad/sudoku/Grid.java
@@ -75,11 +75,11 @@ public class Grid {
     return new Grid(cells);
   }
 
-  public static Grid emptyGrid(){
+  public static Grid emptyGrid() {
     int[][] emptyGrid = new int[9][9];
     return Grid.of(emptyGrid);
   }
-  
+
   public int getSize() {
     return grid.length;
   }
@@ -140,6 +140,10 @@ public class Grid {
     return getNextEmptyCellOf(firstCell);
   }
 
+  @Override public String toString() {
+    return StringConverter.toString(this);
+  }
+
   public static class Cell {
     private int value;
     private Collection<Cell> rowNeighbors;
@@ -193,6 +197,70 @@ public class Grid {
 
     public void setNextCell(Cell nextCell) {
       this.nextCell = nextCell;
+    }
+  }
+
+  private static class StringConverter {
+    public static String toString(Grid grid) {
+      StringBuilder builder = new StringBuilder();
+      int size = grid.getSize();
+
+      printTopBorder(builder);
+      for (int row = 0; row < size; row++) {
+        printRowBorder(builder);
+        for (int column = 0; column < size; column++) {
+          printValue(builder, grid, row, column);
+          printRightColumnBorder(builder, column + 1, size);
+        }
+        printRowBorder(builder);
+        System.out.println();
+        printBottomRowBorder(builder, row + 1, size);
+      }
+      printBottomBorder(builder);
+
+      return builder.toString();
+    }
+
+    private static void printTopBorder(StringBuilder builder) {
+      builder.append("╔═══╤═══╤═══╦═══╤═══╤═══╦═══╤═══╤═══╗\n");
+    }
+
+    private static void printRowBorder(StringBuilder builder) {
+      builder.append("║\n");
+    }
+
+    private static void printValue(StringBuilder builder, Grid grid, int row, int column) {
+      int value = grid.getCell(row, column).getValue();
+      String output = value != 0 ? String.valueOf(value) : " ";
+      builder.append(" " + output + " ");
+    }
+
+    private static void printRightColumnBorder(StringBuilder builder, int column, int size) {
+      if (column == size) {
+        return;
+      }
+
+      if (column % 3 == 0) {
+        builder.append("║");
+      } else {
+        builder.append("│");
+      }
+    }
+
+    private static void printBottomRowBorder(StringBuilder builder, int row, int size) {
+      if (row == size) {
+        return;
+      }
+
+      if (row % 3 == 0) {
+        builder.append("╠═══╪═══╪═══╬═══╪═══╪═══╬═══╪═══╪═══╣\n");
+      } else {
+        builder.append("╟───┼───┼───╫───┼───┼───╫───┼───┼───╢\n");
+      }
+    }
+
+    private static void printBottomBorder(StringBuilder builder) {
+      builder.append("╚═══╧═══╧═══╩═══╧═══╧═══╩═══╧═══╧═══╝\n");
     }
   }
 }
