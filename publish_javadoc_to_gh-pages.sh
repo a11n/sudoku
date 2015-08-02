b3c3a6baf058171a4ec2d486a7d2bb0719834623
a11n <andre.diermann@gmail.com>
2015-08-02 09:57:04 +0200

    Added shell script to publish javadoc to gh-pages branch.

---
+++ publish_javadoc_to_gh-pages.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo "1. Generate javadoc"
+./gradlew javadoc
+
+echo "2. Checkout and clean gh-pages"
+REPO=https://github.com/a11n/sudoku.git
+BRANCH=gh-pages
+FOLDER=$BRANCH
+
+git clone -q $REPO --branch $BRANCH --single-branch $FOLDER
+cd $FOLDER
+git rm -rf . > /dev/null
+
+echo "3. Copy javdoc and deploy"
+cp -r ./build/docs/javadoc/. ./
+git add .
+git commit -m "Published javadoc." > /dev/null
+git push -q
+
+echo "4. Clean-up"
+cd ..
+rm -rf $FOLDER
